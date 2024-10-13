809bac4769ab6c181c5a2e26942f140c0710ed2c
Ankur Patel <ankurnpatel106@gmail.com>
2024-10-12 21:04:45 -0700

    memory_agent

--- agent/lgraph.py
+++ agent/lgraph.py
@@ -1,29 +1,34 @@
-# from langchain.vectorstores import Qdrant
-from langchain_openai import OpenAI, OpenAIEmbeddings
-from langchain_qdrant import QdrantVectorStore
-from qdrant_client import QdrantClient
-from qdrant_client.http.models import Distance, VectorParams
+# import bs4
+from langchain.tools.retriever import create_retriever_tool
+from langchain_community.document_loaders import WebBaseLoader
+from langchain_core.vectorstores import InMemoryVectorStore
+from langchain_openai import ChatOpenAI, OpenAIEmbeddings
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langgraph.checkpoint.memory import MemorySaver
 from langgraph.prebuilt import create_react_agent
+from qdrant_client import QdrantClient
+from langchain_qdrant import QdrantVectorStore
+
+
+
 
 import os
 from dotenv import load_dotenv
-
-# Load environment variables from the .env file
 load_dotenv()
+memory = MemorySaver()
+llm = ChatOpenAI(model="gpt-4o-mini", temperature=0, openai_api_key=os.getenv('OPENAI_API_KEY'))
 
-openai_llm = OpenAI(
-    temperature=0.7, 
-    model="gpt-3.5-turbo", 
-    openai_api_key=os.getenv('OPENAI_API_KEY')
-)
+
 embedding_model = OpenAIEmbeddings(
     openai_api_key=os.getenv('OPENAI_API_KEY')
 )
-print(embedding_model)
+# print(embedding_model)
 qdrant_client = QdrantClient(url = os.getenv('QDRANT_ENDPOINT'), api_key = os.getenv('QDRANT_API_KEY'))
 qdrant_vector_store = QdrantVectorStore(client = qdrant_client, collection_name = "Heritage_Square",embedding = embedding_model)
 
-
+# vectorstore = InMemoryVectorStore.from_documents(
+#     documents=splits, embedding=OpenAIEmbeddings()
+# )
 retriever = qdrant_vector_store.as_retriever()
 
 
@@ -36,7 +41,8 @@ tool = create_retriever_tool(
 tools = [tool]
 
 
-graph = create_react_agent(llm, tools)
+graph = create_react_agent(llm, tools, checkpointer=memory)
+config = {"configurable": {"thread_id": "abc123"}} 
 
 inputs = {"messages": [("user", "What type of documents you have?")]}
 for s in graph.stream(inputs, stream_mode="values"):
@@ -45,4 +51,3 @@ for s in graph.stream(inputs, stream_mode="values"):
         print(message)
     else:
         message.pretty_print()
-
